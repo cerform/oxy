16d51facbfbfba955fa8eef0ca11e285ce920551
cerform <49022097+cerform@users.noreply.github.com>
2025-03-03 16:54:26 +0200

    1.12

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -1,26 +1,20 @@
 import os
-import json
-import base64
 import gspread
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 SHEET_NAME = "Nitrogen_Usage_Log"
 
 def connect_to_sheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-    # Читаем JSON из переменной окружения
-    credentials_base64 = os.getenv("GOOGLE_CREDENTIALS_BASE64")
+    # Путь к секретному файлу
+    creds_path = '/etc/secrets/credentials.json'
 
-    if not credentials_base64:
-        raise FileNotFoundError("Переменная GOOGLE_CREDENTIALS_BASE64 не найдена!")
-
-    # Декодируем base64 в JSON
-    credentials_json = base64.b64decode(credentials_base64).decode("utf-8")
-    creds_dict = json.loads(credentials_json)
+    if not os.path.exists(creds_path):
+        raise FileNotFoundError(f"Секретный файл не найден по пути {creds_path}")
 
     # Создаем учетные данные Google
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+    creds = Credentials.from_service_account_file(creds_path, scopes=scope)
     client = gspread.authorize(creds)
 
     return client.open(SHEET_NAME).sheet1
