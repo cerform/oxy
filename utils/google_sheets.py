1f607162d25c4d93982a6704b5f996d8a155da95
cerform <49022097+cerform@users.noreply.github.com>
2025-03-03 16:10:48 +0200

    Добавлена поддержка GOOGLE_CREDENTIALS_BASE64

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -1,5 +1,6 @@
 import os
 import json
+import base64
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
@@ -8,16 +9,21 @@ SHEET_NAME = "Nitrogen_Usage_Log"
 def connect_to_sheets():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 
-    credentials_json = os.getenv("GOOGLE_CREDENTIALS")  # Берем JSON из переменной окружения
+    # Читаем JSON из переменной окружения
+    credentials_base64 = os.getenv("GOOGLE_CREDENTIALS_BASE64")
 
-    if not credentials_json:
-        raise FileNotFoundError("Переменная GOOGLE_CREDENTIALS не найдена!")
+    if not credentials_base64:
+        raise FileNotFoundError("Переменная GOOGLE_CREDENTIALS_BASE64 не найдена!")
 
-    credentials_dict = json.loads(credentials_json)  # Преобразуем JSON-строку в словарь
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)  # Создаем креды
+    # Декодируем base64 в JSON
+    credentials_json = base64.b64decode(credentials_base64).decode("utf-8")
+    creds_dict = json.loads(credentials_json)
+
+    # Создаем учетные данные Google
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
 
-    return client.open(SHEET_NAME).sheet1  # Открываем Google Sheets
+    return client.open(SHEET_NAME).sheet1
 
 def add_entry_to_sheets(data):
     sheet = connect_to_sheets()
